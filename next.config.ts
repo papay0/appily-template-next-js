fd395a0b399f49606e5cf1abb72de0d4cba0d777
Arthur Papailhau <papailhau@gmail.com>
2025-04-03 22:43:17 -0700

    Fixes build for next.config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '',
-  assetPrefix: './',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
